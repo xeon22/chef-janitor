14bf39f1f6da56ab7c8192cd1ed73f433d6bc289
Mark Pimentel <markp@toqpmac044.quickplay.local>
2012-11-12 09:43:46 -0500

    - bumped version of cookbook for update

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email "markpimentel22@gmail.com"
 license          "Apache 2.0"
 description      "A General Cookbook used to cleanup files and directories on nodes"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version    "0.0.2"
+version    "0.0.3"
 
 # Should work on any of the linuxes
 # Have not tested windows, however all path references have been abstracted
