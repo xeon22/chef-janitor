ad97d9043c9c9696c88776d094c5a74e08b5a8c8
Arie Plugge <aplugge@schubergphilis.com>
2014-07-18 09:23:51 +0200

    Some more cleanup ...

--- recipes/cleanup.rb
+++ recipes/cleanup.rb
@@ -5,20 +5,19 @@
 # Copyright 2014, Schuberg Philis
 #
 
-if !node['janitor'].nil? and !node['janitor']['directory'].nil?
-
+if !node['janitor'].nil? and !node['janitor']['directory'].nil? and !node['janitor']['directory'].empty?
   Chef::Log.info("janitor: #{node['janitor']['directory'].length} entries to process")
-  node['janitor']['directory'].sort.each do |jan_info|
-    jan = jan_info.last
-    janitor_directory "#{jan['path']}" do
-      age		jan['age'] unless jan['age'].nil?
-      size		jan['size'] unless jan['size'].nil?
-      include_only	!jan['include_only'].nil? ? jan['include_only'] : []
-      exclude_all	!jan['exclude_all'].nil?  ? jan['exclude_all']  : []
-      recursive		!jan['recursive'].nil?    ? jan['recursive']    : false
+  node['janitor']['directory'].each do |dir|
+    name = dir.first
+    conf = dir.last
+
+    janitor_directory "#{conf['path']}" do
+      age		conf['age']		unless conf['age'].nil?
+      size		conf['size']		unless conf['size'].nil?
+      include_only	conf['include_only']	unless conf['include_only'].nil?
+      exclude_all	conf['exclude_all']	unless conf['exclude_all'].nil?
+      recursive		conf['recursive']	unless conf['recursive'].nil?
       action		:purge
     end
-
   end
-
 end
