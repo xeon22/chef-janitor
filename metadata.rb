1f842fbd536f71ccda6dcef8f18a7d9b73b3461e
Mark Pimentel <mark.pimentel@pointclickcare.com>
2014-12-02 10:00:22 -0500

    Version bump

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email "markpimentel22@gmail.com"
 license          "Apache 2.0"
 description      "A General Cookbook used to cleanup files and directories on nodes"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version    "1.1.0"
+version    "2.0.0"
 
 # Should work on any of the linuxes
 # Have not tested windows, however all path references have been abstracted
